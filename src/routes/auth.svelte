edf9f6b0d6d58503551129a201514d97f1a5f5be
Imran <imranchowdy@gmail.com>
2022-04-03 17:52:43 +0100

    changed max width on auth page for desktop view

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -25,7 +25,7 @@
 
 <div class="flex h-screen w-screen lg:fixed top-0 lg:z-50 bg-white">
 	<div class="hidden lg:block bg-[url('/authBg.png')] w-1/6 min-h-max rounded-l-md" />
-	<div class="lg:w-1/3 lg:px-4 ">
+	<div class="lg:w-2/5 lg:px-4 ">
 		{#if authState === 'SIGN-UP'}
 			<SignUp on:handleAuthState={handleAuthState} />
 		{:else}
