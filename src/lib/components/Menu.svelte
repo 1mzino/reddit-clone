67f23a0da2be53efaa288eb881e5d1fc0f77e5c0
Imran <imranchowdy@gmail.com>
2022-04-09 17:49:44 +0100

    added desktop menu

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -27,27 +27,24 @@
 	<div class="fixed top-[49px]  lg:hidden flex flex-col h-full w-full">
 		<div on:click={handleMenuClick} class="fixed top-0 h-full w-full bg-black opacity-90 z-20" />
 
-		<div class="h-30 flex flex-col space-y-4 px-3 pt-3 pb-6 bg-redditDarkBlue z-30">
+		<div
+			class="h-30 flex flex-col space-y-4 px-3 pt-3 pb-6 bg-redditDarkBlue dark:bg-zinc-900 z-30"
+		>
 			<input
-				class="w-full bg-gray-700 text-gray-300 py-2 pl-6 text-sm rounded-full focus-visible:outline-none"
+				class="w-full bg-gray-700 dark:bg-zinc-800 text-gray-300 py-2 pl-6 text-sm rounded-full focus-visible:outline-none"
 				placeholder="Search Reddit"
 			/>
 
-			{#if !$session?.user.authenticated}
-				<button
-					on:click={handleSignUp}
-					class="w-full bg-blue-500 py-2 text-sm text-white font-semibold rounded-full"
-				>
+			<button
+				on:click={!$session.user.authenticated ? handleSignUp : handleSignOut}
+				class="w-full bg-blue-500 dark:bg-zinc-300 py-2 text-sm text-white dark:text-zinc-900 font-semibold rounded-full"
+			>
+				{#if !$session?.user.authenticated}
 					Sign up or Log in
-				</button>
-			{:else}
-				<button
-					on:click={handleSignOut}
-					class="w-full bg-blue-500 py-2 text-sm text-white font-semibold rounded-full"
-				>
+				{:else}
 					Log out
-				</button>
-			{/if}
+				{/if}
+			</button>
 		</div>
 	</div>
 {/if}
